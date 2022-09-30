09ec1ea4b83fc69e8145ccdc102316155bc528f3
pjhusky <peter.houska@gmail.com>
2022-09-30 04:53:12 +0200

    Update merge_to_single_html.bat

--- merge_to_single_html.bat
+++ merge_to_single_html.bat
@@ -1,10 +1,9 @@
-@echo pass 'GLSL' or 'WGSL' as an argument
+@echo NOTE: alwas pass either 'GLSL' or 'WGSL' as an argument
 
-python3 html_merger.py index_cpp_input.html
+@python3 html_merger.py index_cpp_input.html
+@pcpp pocketpt_ne.js -D %1 --line-directive > pocketpt_pcpp.js
+@pcpp index_cpp_input_merged.html --line-directive > index_cpp_output_%1.html
+@echo Wrote output to 'index_cpp_output_%1.html'
 
-pcpp pocketpt_ne.js -D %1 --line-directive > pocketpt_pcpp.js
-
-pcpp index_cpp_input_merged.html --line-directive > index_cpp_output_%1.html
-
-del /Q pocketpt_pcpp.js
-del /Q index_cpp_input_merged.html
+@del /Q pocketpt_pcpp.js
+@del /Q index_cpp_input_merged.html
